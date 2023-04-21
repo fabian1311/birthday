4baa9335a015520f2b6475d82a6d46228c8617f8
Alex Carvajal <alexcarvajal@unicauca.edu.co>
2023-04-21 16:42:36 -0500

    fix Route

--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css'
-import './../styles/MyCalendar.css'
+import 'react-calendar/dist/Calendar.css';
+import { Link } from 'react-router-dom';
+import './../styles/MyCalendar.css';
+
 function MyCalendar() {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateSelect = (date) => {
     setSelectedDate(date);
-    const selectedMonth = date.getMonth();
-    const selectedDay = date.getDate();
-    if (selectedMonth === 3 && selectedDay === 26) {
-      window.location.href = '/dashboard'; // Cambia la URL actual del navegador a la URL del componente Dashboard
-    }
   };
 
   return (
-    <div className="Calendar">
-      <Calendar onChange={handleDateSelect} value={selectedDate} />
+    <div className="CalendarContainer">
+      <Calendar onChange={handleDateSelect} value={selectedDate} className="Calendar" />
+      {selectedDate.getMonth() === 3 && selectedDate.getDate() === 26 && (
+        <div className="DashboardButtonContainer">
+          <Link to="/dashboard" className="DashboardButton">Ir al Dashboard</Link>
+        </div>
+      )}
     </div>
   );
 }
