3ada2d3cc71dd625bd9474e6d9f5a150d195f671
Alex Carvajal <alexcarvajal@unicauca.edu.co>
2023-04-22 00:00:50 -0500

    fix carrousel photos npm js

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,45 +1,88 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import image1 from "./../../public/assets/img1.png"
-import image2 from "./../../public/assets/img2.png";
-import image3 from "./../../public/assets/img3.jpg";
-import image4 from "./../../public/assets/img4.png";
-import image5 from "./../../public/assets/img5.png";
-import image6 from "./../../public/assets/img6.jpg";
-import image7 from "./../../public/assets/img7.jpg";
-import image8 from "./../../public/assets/img8.jpg";
-import image9 from "./../../public/assets/img9.jpg";
-import image10 from "./../../public/assets/img10.jpg";
-import image11 from "./../../public/assets/img11.jpg";
-import gif1 from "./../../public/assets/gif1.gif";
-import gif2 from "./../../public/assets/gif2.gif";
-import gif3 from "./../../public/assets/gif3.gif";
-import gif4 from "./../../public/assets/gif4.gif";
 
-import "./../styles/carousel.css"
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import image1 from "./../assets/img1.png"
+import image2 from "./../assets/img2.png";
+import image3 from "./../assets/img3.jpg";
+import image4 from "./../assets/img4.png";
+import image5 from "./../assets/img5.png";
+import image6 from "./../assets/img6.jpg";
+import image7 from "./../assets/img7.jpg";
+import image8 from "./../assets/img8.jpg";
+import image9 from "./../assets/img9.jpg";
+import image10 from "./../assets/img10.jpg";
+import image11 from "./../assets/img11.jpg";
+import gif1 from "./../assets/gif1.gif";
+import gif2 from "./../assets/gif2.gif";
+import gif3 from "./../assets/gif3.gif";
+import gif4 from "./../assets/gif4.gif";
 
 
-function MyCarousel(){
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain', // la imagen se ajusta al tamaño del contenedor
+  backgroundRepeat: 'no-repeat',
+  height: '100vh'
+}
 
-    const imageList =[
-        image1,
-        image2,
-        image3,
-        image4,
-        image5,
-        image6,
-        image7,
-        image8,
-        image9,
-        image10,
-        image11,
-        gif1,
-        gif2,
-        gif3,
-        gif4
-        ];
+const imageList =[
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  gif1,
+  gif2,
+  gif3,
+  gif4
+  ];
 
+  const slideImages = [
+    {
+      url:image1,
+    },{
+      url:image2,
+    },{
+      url:image3,
+    },{
+      url:image4,
+    },{
+      url:image5,
+    },{
+      url:image6,
+    },{
+      url:image7,
+    },{
+      url:image8,
+    },{
+      url:image9,
+    },{
+      url:image10,
+    },{
+      url:image11,
+    },{
+      url:gif1,
+    },{
+      url:gif2,
+    },{
+      url:gif3,
+    },{
+      url:gif4,
+    },
+   
+  ];
+
+const MyCarousel= ()=>{
+ /* 
     const settings ={
         dots:true,
         infinite:true,
@@ -79,8 +122,20 @@ function MyCarousel(){
              ))}
            </Slider>
         </div>
-      );
+      ); */
 
+      return(
+        <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+      )
 }
 
 export default MyCarousel;
