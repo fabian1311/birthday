60e6a77228218171ebf17e9784fb0d272ce68ccd
Alex Carvajal <alexcarvajal@unicauca.edu.co>
2023-04-21 23:23:25 -0500

    Carou

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,21 +1,21 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import image1 from "./../assets/img1.png";
-import image2 from "./../assets/img2.png";
-import image3 from "./../assets/img3.jpg";
-import image4 from "./../assets/img4.png";
-import image5 from "./../assets/img5.png";
-import image6 from "./../assets/img6.jpg";
-import image7 from "./../assets/img7.jpg";
-import image8 from "./../assets/img8.jpg";
-import image9 from "./../assets/img9.jpg";
-import image10 from "./../assets/img10.jpg";
-import image11 from "./../assets/img11.jpg";
-import gif1 from "./../assets/gif1.gif";
-import gif2 from "./../assets/gif2.gif";
-import gif3 from "./../assets/gif3.gif";
-import gif4 from "./../assets/gif4.gif";
+import image1 from "./../../public/assets/img1.png"
+import image2 from "./../../public/assets/img2.png";
+import image3 from "./../../public/assets/img3.jpg";
+import image4 from "./../../public/assets/img4.png";
+import image5 from "./../../public/assets/img5.png";
+import image6 from "./../../public/assets/img6.jpg";
+import image7 from "./../../public/assets/img7.jpg";
+import image8 from "./../../public/assets/img8.jpg";
+import image9 from "./../../public/assets/img9.jpg";
+import image10 from "./../../public/assets/img10.jpg";
+import image11 from "./../../public/assets/img11.jpg";
+import gif1 from "./../../public/assets/gif1.gif";
+import gif2 from "./../../public/assets/gif2.gif";
+import gif3 from "./../../public/assets/gif3.gif";
+import gif4 from "./../../public/assets/gif4.gif";
 
 import "./../styles/carousel.css"
 
